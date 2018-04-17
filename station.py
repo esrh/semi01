54713e148d9eec3dc59013c722c24798c706fb50
esrh <esrh@users.noreply.github.com>
2018-04-18 02:26:29 +0900

    ah

--- station.py
+++ station.py
@@ -1,16 +1,54 @@
 import json, csv, sys
 import simplekml
 import numpy as np
+import xlrd
 
 class nazo:
-
     def __init__(self):
-        self.path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
-    
+        self.testpath = 'G:\\data\\geography\\test\\'
+        self.ntbl = []
+        with open("漢数字テーブル.csv", mode='r') as f:
+            x = f.read().split('\n')
+        for xx in x:
+            self.ntbl.append(xx.split(","))
+        print(self.ntbl)
+        
+    def xls2csv(self, path):
+        data = []
+        book = xlrd.open_workbook(path)
+        a = book.sheet_by_index(0)
+        for row in range(a.nrows):
+            aaa = []
+            for col in range(a.ncols):
+                aaa.append(a.cell(row, col).value)
+            #print(aaa)
+            data.append(aaa)
+        with open("temp\\temp.csv", 'w', encoding='utf8') as f:
+            ff = csv.writer(f, lineterminator='\n')
+            ff.writerows(data)
+        return data
+        
+    def choume(self, data, data2):
+        col = [row[5] for row in data2]
+        lses = []
+        for i, x in enumerate(data):
+            xx = x[2]
+            if xx != "":
+                for ii in range(0,9):#0-8
+                    xx = xx.replace(self.ntbl[0][ii], self.ntbl[1][ii])
+                xx = xx.split('\n')[0]
+                n = col.index(xx)
+                ls = [xx, data2[n][7], data2[n][6]]
+            else:
+                ls = []
+            lses.append(ls)
+        print(lses)
+            
     def station(self, company="京王電鉄"):
+        path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
         kml = simplekml.Kml()
         lses = []
-        with open(self.path, 'r', encoding='utf8') as f:
+        with open(path, 'r', encoding='utf8') as f:
             data = f.read()
         data = json.loads(data)
         for x in data["features"]:
@@ -27,13 +65,24 @@ class nazo:
 
         kml.save("stationdata\\{}.kml".format(company))
         print(lses)
-        with open("stationdata\\{}.csv".format(company), 'w') as f:
+        with open("stationdata\\{}.csv".format(company),encoding="utf8", mode='w') as f:
             ff = csv.writer(f, lineterminator='\n')
             ff.writerows(lses)
 
 if __name__ == '__main__':
     nazo = nazo()
-    company = input(" > ")
-    if company == '':
-        company = '山万'
-    nazo.station(company=company)
+    i = int(input(""))
+    if i == 1:
+        company = input(" > ")
+        if company == '':
+            company = '山万'
+        nazo.station(company=company)
+    if i == 2:
+        path = r'G:\data\geography\test\町丁目\多摩市\多摩市小売.xls'
+        data = nazo.xls2csv(path=path)
+        path = r'G:\data\geography\test\13224-10.0b\13224_2016.csv'
+        data2 = []
+        with open(path, 'r') as f:
+            for row in csv.reader(f):
+                data2.append(row)
+        nazo.choume(data,data2)
