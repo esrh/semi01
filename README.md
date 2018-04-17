2b884ccb984cdc17dbb3d12e2abb958902e3ab05
esrh <esrh@users.noreply.github.com>
2018-04-18 01:02:55 +0900

    eeee

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# semi01
+# semi01 
+# station.py 
+nazoクラスの関数stationで国土数値情報geojsonから任意の会社を抽出
--- station.py
+++ station.py
@@ -1,28 +1,39 @@
-import json, csv
+import json, csv, sys
 import simplekml
 import numpy as np
 
-kml = simplekml.Kml()
-lses = []
+class nazo:
 
-path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
-with open(path, 'r', encoding='utf8') as f:
-    data = f.read()
-data = json.loads(data)
-for x in data["features"]:
-    if x['properties']['運営会社'] == '京王電鉄':
-        sta = x["geometry"]["coordinates"]
-        sta_name = x["properties"]["駅名"]
-        kml.newlinestring(name=sta_name, coords=sta)
+    def __init__(self):
+        self.path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
+    
+    def station(self, company="京王電鉄"):
+        kml = simplekml.Kml()
+        lses = []
+        with open(self.path, 'r', encoding='utf8') as f:
+            data = f.read()
+        data = json.loads(data)
+        for x in data["features"]:
+            if x['properties']['運営会社'] == company:
+                sta = x["geometry"]["coordinates"]
+                sta_name = x["properties"]["駅名"]
+                kml.newlinestring(name=sta_name, coords=sta)
 
-        sta2 = np.mean(sta, axis=0).tolist()
-        p = kml.newpoint(name=sta_name)
-        p.coords = [(sta2[0], sta2[1])]
+                sta2 = np.mean(sta, axis=0).tolist()
+                p = kml.newpoint(name=sta_name)
+                p.coords = [(sta2[0], sta2[1])]
 
-        lses.append([sta_name, sta2[0], sta2[1]])
+                lses.append([sta_name, sta2[0], sta2[1]])
 
-kml.save("keio.kml")
-print(lses)
-with open("keio.csv", 'w') as f:
-    ff = csv.writer(f, lineterminator='\n')
-    ff.writerows(lses)
+        kml.save("stationdata\\{}.kml".format(company))
+        print(lses)
+        with open("stationdata\\{}.csv".format(company), 'w') as f:
+            ff = csv.writer(f, lineterminator='\n')
+            ff.writerows(lses)
+
+if __name__ == '__main__':
+    nazo = nazo()
+    company = input(" > ")
+    if company == '':
+        company = '山万'
+    nazo.station(company=company)
