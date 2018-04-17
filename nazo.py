789e739b0d70a3420e13318e1f73f366143a6126
esrh <esrh@users.noreply.github.com>
2018-04-18 04:44:26 +0900

    aaaa

--- nazo.py
+++ nazo.py
@@ -7,12 +7,26 @@ class nazo:
     def __init__(self):
         self.testpath = 'G:\\data\\geography\\test\\'
         self.ntbl = []
+        #漢数字テーブルはSJISです
         with open("漢数字テーブル.csv", mode='r') as f:
             x = f.read().split('\n')
         for xx in x:
             self.ntbl.append(xx.split(","))
         print(self.ntbl)
         
+    def wcsv(self, path, data, encoding='utf8'):
+        with open(path, 'w', encoding=encoding) as f:
+            ff = csv.writer(f, lineterminator='\n')
+            ff.writerows(data)
+
+    def rcsv(self, path, encoding='utf8'):
+        data = []
+        with open(path, 'r', encoding=encoding) as f:
+            for row in csv.reader(f):
+                data.append(row)
+        return data
+        
+    
     def xls2csv(self, path):
         data = []
         book = xlrd.open_workbook(path)
@@ -28,22 +42,34 @@ class nazo:
             ff.writerows(data)
         return data
         
-    def choume(self, data, data2):
+    def choume(self, data, data2):#調べるデータ、町丁目座標データ、の順
         col = [row[5] for row in data2]
         lses = []
         for i, x in enumerate(data):
-            xx = x[2]
+            if x[1] == "":
+                xx = x[2]
+            elif data[i+1][2] == "":
+                xx = x[1]
+            else:
+                xx = ''
             if xx != "":
                 for ii in range(0,9):#0-8
                     xx = xx.replace(self.ntbl[0][ii], self.ntbl[1][ii])
                 xx = xx.split('\n')[0]
-                n = col.index(xx)
-                ls = [xx, data2[n][7], data2[n][6]]
+                try:
+                    n = col.index(xx)
+                    nn = x[8]
+                    if type(nn) != float:
+                        nn = ''
+                    ls = [xx, data2[n][7], data2[n][6], nn]
+                except ValueError:
+                    ls = []
             else:
                 ls = []
             lses.append(ls)
-        print(lses)
-            
+        self.wcsv('temp\\temp2.csv', lses)
+        return lses
+        
     def station(self, company="京王電鉄"):
         path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
         kml = simplekml.Kml()
@@ -68,6 +94,7 @@ class nazo:
         with open("stationdata\\{}.csv".format(company),encoding="utf8", mode='w') as f:
             ff = csv.writer(f, lineterminator='\n')
             ff.writerows(lses)
+        return lses
 
 if __name__ == '__main__':
     nazo = nazo()
@@ -78,11 +105,11 @@ if __name__ == '__main__':
             company = '山万'
         nazo.station(company=company)
     if i == 2:
+        ftbl = nazo.rcsv("ファイルテーブル.csv")#utf8,ヘッダあり
+        
         path = r'G:\data\geography\test\町丁目\多摩市\多摩市小売.xls'
         data = nazo.xls2csv(path=path)
         path = r'G:\data\geography\test\13224-10.0b\13224_2016.csv'
-        data2 = []
-        with open(path, 'r') as f:
-            for row in csv.reader(f):
-                data2.append(row)
-        nazo.choume(data,data2)
+        data2 = nazo.rcsv(path, encording='sjis')
+        data3 = nazo.choume(data,data2)
+        print(data3)
