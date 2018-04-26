f1085e5e6303a6b18c6789ddef2dac1f2757f434
esrh <esrh@users.noreply.github.com>
2018-04-27 01:44:37 +0900

    aaaa

--- postgis01.py
+++ postgis01.py
@@ -1,4 +1,7 @@
 from postgis_module import pg
+import nazo
+import glob, time, csv
+
 class pg(pg):
     def __init__(self, dbname='gistest'):
         super(pg, self).__init__(dbname=dbname)
@@ -6,12 +9,13 @@ class pg(pg):
         for x in self.pg:
             print(x)
 
-    def chk_srid(self):
-        self.pg.execute('select ST_SRID(geom) from tokyo_syogyo limit 1')
+    def chk_srid(self, tbname='tokyo_syogyo'):
+        self.pg.execute('select ST_SRID(geom) from {} limit 1'.format(tbname))
         for x in self.pg:
             print(x)
 
-    def nazo01(self, lon='139.777254', lat='35.713768', kyori='1000.0'):
+    def dwithin(self, lon='139.777254', lat='35.713768', kyori='1000.0'):
+        debug = False
         pg = self.pg
         #ST_Distance('SRID=4612;POINT({0} {1})', geom) as dist
         a="""
@@ -28,17 +32,38 @@ class pg(pg):
         ORDER BY dist;
         """.format(lon, lat, kyori).replace("\n        ", '\n')
         pg.execute(a)
-        xx = []
+        ls = []
+        ls_kouri = []
         uriage = 0
         for x in pg:
-            print(x)
-            xx.append(round(x[2]))
-            uriage = uriage + x[3]
-        print(xx, uriage)
-        
+            ls_kouri.append('{}:{}'.format(x[0], x[3]))
+            if debug is True:
+                print(x)
+            ls.append(round(x[2]))
+            try:
+                uriage = uriage + x[3]
+            except TypeError:
+                pass
+        if debug is True:
+            print(ls, uriage)
+        return uriage, ls_kouri
+
+    def nazo01(self, ls=None, kyori=None):
+        rl = []
+        for x in ls:
+            i, ls_kouri = self.dwithin(lon=x[1], lat=x[2], kyori=kyori)
+            rl.append(x + [i] + ls_kouri)
+        return rl
 
 if __name__ == '__main__':
+    nazo = nazo.nazo()
     pg = pg(dbname='gistest')
     pg.chk_srid()
-    lon, lat = [139.69861500000002,35.68843]
-    pg.nazo01(lon, lat, '1000')
+    #lon, lat = [139.69861500000002,35.68843]
+    rls = []
+    for x in glob.glob('stationdata\\tokyo\\*.csv'):
+        ls = nazo.rcsv(x)
+        rl = pg.nazo01(ls, kyori=1000)
+        print(x)
+        rls = rls + rl
+    nazo.wcsv('stadata_tokyo_003.csv', rls)
